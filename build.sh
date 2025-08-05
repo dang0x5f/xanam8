284274f20743eb5f1be090eb77806b8023ed9de6
dang0x5f <d4n9xcv@gmail.com>
2025-08-04 21:57:40 -0400

    added: file gitignore

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cc -o grid --std=c2x -I/usr/local/include -D_THREAD_SAFE -L/usr/local/lib -lX11 main.c
+cc -o prog --std=c2x -I/usr/local/include -D_THREAD_SAFE -L/usr/local/lib -lX11 main.c
